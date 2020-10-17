3273b4b2da91348ade34a33c6f04006036d7bb01
Tanmay Gujar <tanmaygujar999@gmail.com>
2020-10-17 10:19:32 +0530

    added functionality for mouse and touch events

--- main.js
+++ main.js
@@ -40,3 +40,78 @@ function elt(type, props, ...children) {
     }
     return elem;
 }
+
+const scale = 10;
+
+class PictureCanvas {
+    constructor(picture, pointerDown) {
+        this.dom = elt("canvas", {
+            onmousedown: event => this.mouse(event, pointerDown),
+            ontouchstart: event => this.touch(event, pointerDown) 
+        });
+        this.syncState(picture);
+    }
+    mouse(event, onDown) {
+        if (event.button !== 0) return;
+        let pos = pointerPosition(event, this.dom);
+        let onMove = onDown(pos);
+        if (!onMove) return;
+        let move = moveEvent => {
+            if (mouseEvent.buttons === 0) {
+                this.dom.removeEventListener("mousemove", move);
+            } else {
+                let newPos = pointerPosition(mouseEvent, this.dom);
+                if (pos.x == newPos.x && pos.y == newPos.y) return;
+                pos = newPos;
+                onMove(pos);
+            }
+        }
+        this.dom.addEventListener("mousemove", move);
+    }
+
+    touch(event, onDown) {
+        let pos = pointerPosition(event.touches[0], this.dom);
+        let onMove = onDown(pos);
+        event.preventDefault();
+        if (!onMove) return;
+        let move = moveEvent => {
+            let newPos = pointerPosition(moveEvent.touches[0], this.dom);
+            if (newPos.x == pos.x && newPos.y == pos.y) return;
+            pos = newPos;
+            onMove(newPos); 
+        }
+        let end = () => {
+            this.dom.removeEventListener("touchmove", move);
+            this.dom.removeEventListener("touchend", end);
+        };
+        this.dom.addEventListener("touchmove", move);
+        this.dom.addEventListener("touchend", end);
+    }
+
+    syncState(picture) {
+        if (picture == this.picture) return;
+        this.picture = picture;
+        drawPicture(this.picture, this.dom, scale)
+    }
+}
+
+function pointerPosition(pos, domNode) {
+    let rect = domNode.getBoundingClientRect();
+    return {
+        x: Math.floor((pos.clientX - rect.left) / scale),
+        y: Math.floor((pos.clientY - rect.top) / scale)
+    }
+}
+
+function drawPicture(picture, canvas, scale) {
+    canvas.width = picture.width * scale;
+    canvas.height = picture.height * scale;
+    let cx = canvas.getContext("2d");
+    for (let x = 0; x < picture.height; x++) {
+        for (let y = 0; y < picture.width; y++) {
+            cx.fillStyle = picture.pixel(x, y);
+            cx.fillRect(x * scale, y * scale, scale, scale);
+        }
+    }
+}
+
