caa07aa017a969a9e2bf221aa644b8e5d74b6e5b
Tanmay Gujar <tanmaygujar999@gmail.com>
2020-10-17 20:08:52 +0530

    added feature to load images

--- main.js
+++ main.js
@@ -233,3 +233,55 @@ class SaveButton {
     syncState(state) { this.picture = state.picture; }
 }
 
+class LoadButton {
+    constructor (_, {dispatch}) {
+        this.dom = elt("button", {
+            onclick: () => startLoad(dispatch)
+        }, "Load");
+    }
+    syncState() {}
+}
+
+function startLoad(dispatch) {
+    let input = elt("input", {
+        type: "file",
+        onchange: () => finishLoad(input.files[0], dispatch)
+    });
+    document.body.appendChild(input);
+    input.click();
+    input.remove();
+}
+
+function finishLoad(file, dispatch) {
+    if (file == null) return;
+    let reader = new FileReader();
+    reader.addEventListener("load", () => {
+        let image = elt("img", {
+            onload: () => dispatch({
+                picture: pictureFromImage(image)
+            }),
+            src: reader.result
+        });
+    });
+    reader.readAsDataURL(file);
+}
+
+function pictureFromImage(image) {
+    let width = Math.min(100, image.width);
+    let height = Math.min(100, image.height);
+    let canvas = elt("canvas", {width, height});
+    let cx = canvas.getContext("2d");
+    cx.drawImage(image, 0, 0);
+    let pixels = [];
+    let {data} = cx.getImageData(0, 0, width, height);
+
+    function hex(n) {
+        return n.toString(16).padStart(2, "0");
+    }
+
+    for (let i = 0; i < data.length; i += 4) {
+        let [r, g, b] = data.slice(i, i + 3);
+        pixels.push("#" + hex(r) + hex(g) + hex(b));
+    }
+    return new Picture(width, height, pixels);
+}
