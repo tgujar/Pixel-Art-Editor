dbf19bf693a25eb67f819aa3f19aed804eb5b9b3
Tanmay Gujar <tanmaygujar999@gmail.com>
2020-10-17 09:05:04 +0530

    added function to create dom nodes

--- main.js
+++ main.js
@@ -22,9 +22,21 @@ class Picture {
     // updates the pixels array with the new values
     draw(pixels) {
         let copy = this.pixels.slice()
+        // pixels must contain x, y and color props
         for (let {x, y, color} of pixels) {
             copy[y * width + x] = color;
         }
         return new Picture(this.width, this.height, copy);
     }   
+}
+
+// create an element of "type", with properties "prop" and "children"
+function elt(type, props, ...children) {
+    let elem = document.createElement(type);
+    if (props) Object.assign(elem, props);
+    for (let child of children) {
+        if (typeof child !== "string") elem.appendChild(child);
+        else elem.appendChild(document.createTextNode(child));
+    }
+    return elem;
 }
