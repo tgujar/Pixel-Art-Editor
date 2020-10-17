cc540f26b26e725e8c1ad673a9df442bf47b4e9c
Tanmay Gujar <tanmaygujar999@gmail.com>
2020-10-17 19:49:45 +0530

    added save feature

--- main.js
+++ main.js
@@ -212,3 +212,24 @@ function pick({x, y}, state, dispatch) {
     dispatch({color: state.picture.pixel(x, y)});
 }
 
+class SaveButton {
+    constructor(state) {
+        this.picture = state.picture;
+        this.dom = elt("button", {
+            onclick: () => this.save()
+        }, "Save");
+    }
+    save() {
+        let canvas = elt("canvas");
+        drawPicture(this.picture, canvas, 1);
+        let link = elt("a", {
+            href: canvas.toDataURL(),
+            download: "pixelart.png"
+        });
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+    }
+    syncState(state) { this.picture = state.picture; }
+}
+
