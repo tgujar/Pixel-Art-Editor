d35bc334dbb7b1b5d89cda3ccac5a2e6360fc469
Tanmay Gujar <tanmaygujar999@gmail.com>
2020-10-18 18:42:45 +0530

    bug fix for rectangle tool

--- main.js
+++ main.js
@@ -173,9 +173,9 @@ function draw(pos, state, dispatch) {
 function rectangle(start, state, dispatch) {
     function drawRectangle(pos) {
         let xStart = Math.min(start.x, pos.x);
-        let yStart = Math.min(start.x, pos.x);
+        let yStart = Math.min(start.y, pos.y);
         let xEnd = Math.max(start.x, pos.x);
-        let yEnd = Math.max(start.x, pos.x);
+        let yEnd = Math.max(start.y, pos.y);
         let drawn = [];
         for (let y = yStart; y <= yEnd; y++) {
             for (let x = xStart; x <= xEnd; x++) {
